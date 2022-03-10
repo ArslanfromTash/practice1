3daeb3cfe9bb6ad71e0aba629b952c43f2a0705b
ArslanfromTash <aakhmerov01@mail.ru>
2022-03-10 14:32:21 +0300

    Init coomit

---
+++ clickbuttons/src/main/java/ru/mirea/akhmerov/clickbuttons/MainActivity.java
@@ -0,0 +1,39 @@
+package ru.mirea.akhmerov.clickbuttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvOut;
+    private Button btnOk;
+    private Button btnCansel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = findViewById(R.id.btnOk);
+        btnCansel = findViewById(R.id.btnCansel);
+
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                tvOut.setText("Нажата кнопка ОК");
+            }
+        };
+
+        View.OnClickListener oclBtnCansel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                tvOut.setText("Нажата кнопка CANSEL");
+            }
+        };
+
+        btnOk.setOnClickListener(oclBtnOk);
+        btnCansel.setOnClickListener(oclBtnCansel);
+    }
+}
